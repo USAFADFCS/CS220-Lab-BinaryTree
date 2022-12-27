bfff0045ace489c92056d97ea5c886ce93459859
Adrian de Freitas <Adefreit@users.noreply.github.com>
2022-12-27 11:44:43 -0700

    Updated template

--- main.c
+++ main.c
@@ -14,42 +14,42 @@ int main() {
 
     // Testing Insert
     root = insertBST(root,5);
-    root = insertBST(root,10);
-    root = insertBST(root,1);
-    root = insertBST(root,7);
-    root = insertBST(root,3);
-    root = insertBST(root,6);
+    //root = insertBST(root,10);
+    //root = insertBST(root,1);
+    //root = insertBST(root,7);
+    //root = insertBST(root,3);
+    //root = insertBST(root,6);
 
     // Testing Traversals
-    printPreorderBST(root);printf("\n");
-    printPostorderBST(root);printf("\n");
-    printInorderBST(root);printf("\n");
+    //printPreorderBST(root);printf("\n");
+    //printPostorderBST(root);printf("\n");
+    //printInorderBST(root);printf("\n");
 
     // Testing Binary Search
-    BSTnode* curNode = searchBST(root,10);
-    if (curNode == NULL) {
-       printf("Error - Not found.\n");
-    } else {
-       printf("found -> %d\n", curNode->data);
-    }
+    //BSTnode* curNode = searchBST(root,10);
+    //if (curNode == NULL) {
+    //   printf("Error - Not found.\n");
+    //} else {
+    //   printf("found -> %d\n", curNode->data);
+    //}
 
     // Testing Contains
-    printf("contains? -> %d\n", containsBST(root,3));
+    //printf("contains? -> %d\n", containsBST(root,3));
 
     // Testing Size
-    printf("size -> %d\n",sizeBST(root));
+    //printf("size -> %d\n",sizeBST(root));
 
     // Testing Height
-    printf("height -> %d\n",heightBST(root));
+    //printf("height -> %d\n",heightBST(root));
 
     // Testing Delete
-    root = deleteNodeBST(root,6);
-    root = deleteNodeBST(root,3);
-    root = deleteNodeBST(root,5);
-    displayBST(root);
+    //root = deleteNodeBST(root,6);
+    //root = deleteNodeBST(root,3);
+    //root = deleteNodeBST(root,5);
+    //displayBST(root);
 
     // Testing Destroy
-    destroyBST(root);
-    displayBST(root);
+    //destroyBST(root);
+    //displayBST(root);
 }
 

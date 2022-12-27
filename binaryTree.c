a768833d489ada37f1b5937a7cb3ea07e7cae11c
Adrian de Freitas <Adefreit@users.noreply.github.com>
2022-12-27 11:32:52 -0700

    Uploading Template

---
+++ binaryTree.c
@@ -0,0 +1,117 @@
+/**
+ * ================================================================
+ * Name:
+ * Section:
+ * Project: Header for BST library
+ * =================================================================
+ */
+#include "binaryTree.h"
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+// May be useful for heightBST
+int Max(int a, int b)
+{
+    if (a >= b)
+    {
+        return a;
+    }
+    return b;
+}
+
+BSTnode *insertBST(BSTnode *node, int item)
+{
+    return NULL;
+}
+
+void printInorderBST(BSTnode *node)
+{
+    
+}
+
+void printPreorderBST(BSTnode *node)
+{
+
+}
+
+void printPostorderBST(BSTnode *node)
+{
+
+}
+
+BSTnode *searchBST(BSTnode *node, int item)
+{
+    return NULL;
+}
+
+bool containsBST(BSTnode *node, int item)
+{
+    return false;
+}
+
+int sizeBST(BSTnode *node)
+{
+    return 0;
+}
+
+int heightBST(BSTnode *curr)
+{
+    return 0;
+}
+
+void destroyBST(BSTnode *node)
+{
+
+}
+
+BSTnode *deleteNodeBST(BSTnode *node, int item)
+{
+    return NULL;
+}
+
+/** Optional Functions for Displaying a BST **/
+void toArray(int *items, const BSTnode *curr, int index)
+{
+    if (curr != NULL)
+    {
+        items[index] = curr->data;
+        toArray(items, curr->left, index * 2 + 1);
+        toArray(items, curr->right, index * 2 + 2);
+    }
+}
+
+void displayBST(BSTnode *node)
+{
+    printf("----- TREE START -----\n");
+    // If the tree is any taller than 6, it will be too wide to display horizontally.
+    int items[64] = {0}; // Max number of items in a binary tree of six levels.
+    toArray(items, node, 0);
+    int lineWidthOfDeepestLevel =
+        (int)pow(2.0, heightBST(node) - 1) * 4; // Two-digit numbers plus two spaces between.
+    int spacesOnCurrentLevel = lineWidthOfDeepestLevel / 2;
+    int itemsOnCurrentLevel = 1;
+    for (int level = 0; level < heightBST(node); level++)
+    {
+        int start = (int)pow(2.0, level) - 1; // Starting index in the array.
+        for (int i = 0; i < itemsOnCurrentLevel; i++)
+        {
+            for (int spaces = 0; spaces < spacesOnCurrentLevel - 2; spaces++)
+                printf(" ");
+            if (items[start + i] > 0)
+                printf("%d", items[start + i]);
+            else
+                printf("  ");
+            for (int spaces = 0; spaces < spacesOnCurrentLevel; spaces++)
+                printf(" ");
+        }
+        printf("\n\n");
+
+        // Increment these variables for the next iteration.
+        spacesOnCurrentLevel /= 2;
+        itemsOnCurrentLevel *= 2;
+    }
+    printf("-----  TREE END  -----\n");
+}
---
+++ main.c
@@ -0,0 +1,55 @@
+/** 
+* ================================================================
+* Name: 
+* Section: 
+* Project: Test Program for BST library
+* =================================================================
+*/
+#include "binaryTree.h"
+
+#include <stdio.h>
+int main() {
+    // Creates an Empty Tree
+    BSTnode* root = NULL;
+
+    // Testing Insert
+    root = insertBST(root,5);
+    root = insertBST(root,10);
+    root = insertBST(root,1);
+    root = insertBST(root,7);
+    root = insertBST(root,3);
+    root = insertBST(root,6);
+
+    // Testing Traversals
+    printPreorderBST(root);printf("\n");
+    printPostorderBST(root);printf("\n");
+    printInorderBST(root);printf("\n");
+
+    // Testing Binary Search
+    BSTnode* curNode = searchBST(root,10);
+    if (curNode == NULL) {
+       printf("Error - Not found.\n");
+    } else {
+       printf("found -> %d\n", curNode->data);
+    }
+
+    // Testing Contains
+    printf("contains? -> %d\n", containsBST(root,3));
+
+    // Testing Size
+    printf("size -> %d\n",sizeBST(root));
+
+    // Testing Height
+    printf("height -> %d\n",heightBST(root));
+
+    // Testing Delete
+    root = deleteNodeBST(root,6);
+    root = deleteNodeBST(root,3);
+    root = deleteNodeBST(root,5);
+    displayBST(root);
+
+    // Testing Destroy
+    destroyBST(root);
+    displayBST(root);
+}
+
